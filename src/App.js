63a64a1163580b16ae1e98f234e634e14e29e976
OukVathanak <oukk.vathanak@gmail.com>
2023-04-04 18:20:45 +0700

    Home/Category page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home'
+import ProductDetail from './pages/ProductDetail';
+import Categories from './pages/Categories';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/products/:id' element={<ProductDetail />}/>
+        <Route path='/categories' element={<Categories />}/>
+        <Route path='/categories/:id' element={<Categories />}/>
+      </Routes>
     </div>
   );
 }
