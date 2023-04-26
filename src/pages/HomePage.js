062593041fcf9c349f3ebe4fa547b123414d597b
SatyaProgrammer <satya.sieng@student.cadt.edu.kh>
2023-04-27 00:52:04 +0700

    navbar and sidebar

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,14 @@
+import React from "react";
+import { FeaturedProducts, Hero, Services, Contact } from "../components";
+
 const HomePage = () => {
-  return <div>okayy</div>;
+  return (
+    <main>
+      <Hero />
+      <FeaturedProducts />
+      <Services />
+      <Contact />
+    </main>
+  );
 };
 export default HomePage;
