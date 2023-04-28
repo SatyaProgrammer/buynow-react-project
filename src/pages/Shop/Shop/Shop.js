22bbef78aef54761bf1e57c1214145c544ca7b9b
OukVathanak <oukk.vathanak@gmail.com>
2023-04-28 11:11:20 +0700

    Shop page Dashboard

---
+++ src/pages/Shop/Shop/Shop.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Shop.css'
+import ShopSidebar from "../ShopSidebar/ShopSidebar";
+
+function Shop() {
+    return (  
+        <>
+            <div className="container">
+                <ShopSidebar />
+            </div>
+        </>
+    );
+}
+
+export default Shop;
