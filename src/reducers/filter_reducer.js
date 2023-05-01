df66feb940c4e29e90d0ef4d6708acbe13f2ca3d
SatyaProgrammer <satya.sieng@student.cadt.edu.kh>
2023-05-01 15:55:27 +0700

    Set up filter products

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -11,10 +11,14 @@ import {
 
 const filter_reducer = (state, action) => {
   if (action.type === LOAD_PRODUCTS) {
+    let maxPrice = action.payload.map((p) => p.price);
+    maxPrice = Math.max(...maxPrice);
+
     return {
       ...state,
       all_products: [...action.payload],
       filtered_products: [...action.payload],
+      filters: { ...state.filters, max_price: maxPrice, price: maxPrice },
     };
   }
   if (action.type === SET_GRIDVIEW) {
@@ -48,7 +52,6 @@ const filter_reducer = (state, action) => {
     return { ...state, filtered_products: tempProducts };
   }
   return state;
-  throw new Error(`No Matching "${action.type}" - action type`);
 };
 
 export default filter_reducer;
