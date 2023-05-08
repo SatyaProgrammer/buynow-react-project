24fe26d5d0bec6c82e999853ec494babd287dfa1
OukVathanak <oukk.vathanak@gmail.com>
2023-05-08 20:11:16 +0700

    Add product page

--- src/pages/Shop/AddProduct/AddProduct.js
+++ src/pages/Shop/AddProduct/AddProduct.js
@@ -1,10 +1,60 @@
 import React from "react";
+import { useReducer, useEffect, useRef } from "react";
+import { addProductReducer, INITIAL_STATE, ACTION_TYPES } from "./AddProductReducer";
+
+const price_regex = /^[0-9\b]+$/
 
 const AddProduct = () => {
+    const [state, dispatch] = useReducer(addProductReducer, INITIAL_STATE)
+    const nameRef = useRef()
+    const priceRef = useRef('')
 
-    const handleChange = (e) => {
-
+    const handleAddCustom = () => {
+        dispatch({type:ACTION_TYPES.ADD_CUSTOMIZATION, payload: {title: '', custom1: ''}})
     }
+
+    const handleChangeCustom = (onChangeValue, i) => {
+        let inputData = state.customization
+        inputData[i]['title'] = onChangeValue.target.value
+        dispatch({type:ACTION_TYPES.SET_CUSTOMIZATION, payload: [inputData]})
+    }
+
+    const handleAddSubCustom = (i) => {
+        let inputData = state.customization
+        let objectSize = Object.keys(state.customization[i]).length
+        inputData[i]["custom" + objectSize] = ''
+        dispatch({type:ACTION_TYPES.SET_CUSTOMIZATION, payload: [inputData]})
+    }
+
+    const handleChangeSubCustom = (onChangeValue, i, k) => {
+        let inputData = state.customization
+        inputData[i]["custom" + (k+1)] = onChangeValue.target.value
+        dispatch({type:ACTION_TYPES.SET_CUSTOMIZATION, payload: [inputData]})
+    }
+
+    if(state.customization && state.customization.length > 0){
+        // console.log(state.customization)
+    }
+
+    useEffect(() => {
+        nameRef.current.focus()
+        dispatch({type:ACTION_TYPES.ADD_CUSTOMIZATION, payload: {title: '', custom1: ''}})
+    },[])
+
+    useEffect(() => {
+        priceRef.current = state.price
+        if(price_regex.test(state.price)){
+            dispatch({type: ACTION_TYPES.SET_PRICE, payload:state.price})
+        }else{
+            dispatch({type: ACTION_TYPES.SET_PRICE, payload:priceRef.current})
+        }
+    }, [state.price])
+
+
+    // console.log(state.name)
+    // console.log(state.description)
+    // console.log(state.brand)
+    // console.log(state.price)
 
     return(
         <>
@@ -14,12 +64,14 @@ const AddProduct = () => {
                 <div className="flex flex-col gap-4">
                     <div className="bg-white p-4 shadow-lg rounded-md flex flex-col gap-4">
                         <div className="flex flex-col gap-2">
-                            <div className="text-xl font-semibold text-cldark">Product Title</div>
+                            <div className="text-xl font-semibold text-cldark">Product Name</div>
                             <input
                                 type="text"
                                 placeholder="Product name"
-                                onChange={handleChange}
+                                ref={nameRef}
+                                onChange={(e) => dispatch({type: ACTION_TYPES.SET_NAME, payload: e.target.value})}
                                 name="name"
+                                value={state.name}
                                 className="border border-gray-300 w-full p-3 rounded-lg text-cldark"
                             />
                         </div>
@@ -28,8 +80,9 @@ const AddProduct = () => {
                             <textarea
                                 type="text"
                                 placeholder="Product description"
-                                onChange={handleChange}
+                                onChange={(e) => dispatch({type: ACTION_TYPES.SET_DESCRIPTION, payload: e.target.value})}
                                 name="description"
+                                value={state.description}
                                 rows="5" 
                                 className="w-full border border-gray-300 rounded-lg p-3 text-cldark"
                             />
@@ -38,25 +91,69 @@ const AddProduct = () => {
 
                     <div className="bg-white p-4 shadow-lg rounded-md flex flex-col gap-4">
                         <div className="flex flex-col gap-2">
-                            <div className="text-xl font-semibold text-cldark">Product Title</div>
+                            <div className="text-xl font-semibold text-cldark">Brand</div>
                             <input
                                 type="text"
-                                placeholder="Product name"
-                                onChange={handleChange}
-                                name="name"
+                                placeholder="Product brand"
+                                onChange={(e) => dispatch({type: ACTION_TYPES.SET_BRAND, payload: e.target.value})}
+                                name="brand"
+                                value={state.brand}
                                 className="border border-gray-300 w-full p-3 rounded-lg text-cldark"
                             />
                         </div>
                         <div className="flex flex-col gap-2">
-                            <div className="text-xl font-semibold text-cldark">Product Description</div>
-                            <textarea
+                            <div className="text-xl font-semibold text-cldark">Price</div>
+                            <input
                                 type="text"
-                                placeholder="Product description"
-                                onChange={handleChange}
-                                name="description"
-                                rows="5" 
-                                className="w-full border border-gray-300 rounded-lg p-3 text-cldark"
+                                ref={priceRef}
+                                placeholder="Product price"
+                                onChange={(e) => dispatch({type: ACTION_TYPES.SET_PRICE, payload: e.target.value})}
+                                name="price"
+                                value={state.price}
+                                className="border border-gray-300 w-full p-3 rounded-lg text-cldark"
                             />
+                        </div>
+
+                        <div className="flex flex-col gap-2">
+                            <div className="text-xl font-semibold text-cldark">
+                                Customization
+                            </div>
+                            {console.log(JSON.stringify(state.customization))}
+                            {state.customization?.map((custom,i) => (
+                                <div key={i} className="grid grid-cols-2 gap-3">
+                                    <div>
+                                        <input
+                                            type="text"
+                                            placeholder="Title"
+                                            onChange={(e) => handleChangeCustom(e,i)}
+                                            name="customTitle"
+                                            className="border border-gray-300 w-full p-3 rounded-lg text-cldark"
+                                        />
+                                    </div>
+                                    <div className="flex flex-col gap-3">
+                                        {Object.entries(state.customization[i]).filter(([key, _]) => key !== "title").map((t,k) => (
+                                            <div key={k}>
+                                                <div className="flex items-center gap-2">
+                                                    {t.custom1}
+                                                    <input
+                                                        type="text"
+                                                        placeholder="Custom list"
+                                                        onChange={(e) => handleChangeSubCustom(e, i, k)}
+                                                        name="subCustom"
+                                                        className="border border-gray-300 w-full p-3 rounded-lg text-cldark"
+                                                    />   
+                                                    <div className="w-8 h-8 border" onClick={() => handleAddSubCustom(i)}></div>  
+                                                </div>
+                                            </div>
+                                        ))}    
+                                    </div>
+
+                                </div>
+                            ))}
+
+                            <div>
+                                <div onClick={handleAddCustom}>Add</div>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/pages/Shop/AddProduct/AddProductReducer.js
+++ src/pages/Shop/AddProduct/AddProductReducer.js
@@ -1,11 +1,73 @@
-import React from "react";
-
-const AddProductReducer = () => {
-    return(
-        <>
-        
-        </>
-    )
+export const ACTION_TYPES = {
+    SET_NAME: 'SET_NAME',
+    SET_NAME_FOCUS: 'SET_NAME_FOCUS',
+    SET_DESCRIPTION: 'SET_DESCRIPTION',
+    SET_DESCRIPTION_FOCUS: 'SET_DESCRIPTION_FOCUS',
+    SET_BRAND: 'SET_BRAND',
+    SET_PRICE: 'SET_PRICE',
+    ADD_CUSTOMIZATION: 'ADD_CUSTOMIZATION',
+    SET_CUSTOMIZATION: 'SET_CUSTOMIZATION',
 }
 
-export default AddProductReducer
+export const INITIAL_STATE = {
+    name: '',
+    nameFocus: false,
+
+    description: '',
+    descriptionFocus: false,
+
+    brand: '',
+    
+    price: '',
+
+    constomization: ([]),
+
+    errMessage: '',
+    sucess: false,
+}
+
+export const addProductReducer = (state, action) => {
+    switch (action.type) {
+        case ACTION_TYPES.SET_NAME:
+            return {
+                ...state,
+                name: action.payload
+            }
+        case ACTION_TYPES.SET_NAME_FOCUS:
+            return {
+                nameFocus: !state.nameFocus
+            }
+        case ACTION_TYPES.SET_DESCRIPTION:
+            return {
+                ...state,
+                description: action.payload
+            }
+        case ACTION_TYPES.SET_DESCRIPTION_FOCUS:
+            return {
+                descriptionFocus: !state.descriptionFocus
+            }
+        case ACTION_TYPES.SET_BRAND:
+            return {
+                ...state,
+                brand: action.payload
+            }
+        case ACTION_TYPES.SET_PRICE:
+            return {
+                ...state,
+                price: action.payload
+            }
+        case ACTION_TYPES.ADD_CUSTOMIZATION:
+            return {
+                ...state,
+                customization: [...state.customization || [], action.payload]
+            }
+        case ACTION_TYPES.SET_CUSTOMIZATION:
+            return {
+                ...state,
+                customElements: action.payload
+            }
+
+        default:
+            return state;
+    }
+}
