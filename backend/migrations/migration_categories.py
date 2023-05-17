d8a8e5dec51457098fa476b15a6b54ad65b7588f
SatyaProgrammer <satya.sieng@student.cadt.edu.kh>
2023-05-17 16:32:55 +0700

    put some info into migration

--- backend/migrations/migration_categories.py
+++ backend/migrations/migration_categories.py
@@ -4,22 +4,28 @@ import mysql.connector as msc
 # don't change this variable name
 TABLE_NAME = "categories"
 
+
 def up():
     table = Table(name=TABLE_NAME)
     table.int("id").primary_key().auto_increment()
     table.varchar("name", 255)
 
-    table.migrate() # always finish with a call to table.migrate()
+    table.migrate()  # always finish with a call to table.migrate()
     return table
-    
+
+
 def down(db_conn: msc.MySQLConnection):
     Table(name=TABLE_NAME).drop(db_conn)
+
 
 # if you need to seed the database
 def seed() -> list[dict]:
     # just return a list of key-value pairs
     return [
-        {
-            "name": "Truck"
-        }
+        {"name": "Office"},
+        {"name": "Living Room"},
+        {"name": "Kitchen"},
+        {"name": "Bedroom"},
+        {"name": "Dining"},
+        {"name": "Kids"},
     ]
