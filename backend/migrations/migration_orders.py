4ffafe5bd32cbbedf7eab867b80a42201ffd982e
T-Thyrak <bus.teng.thaisothyrak@gmail.com>
2023-05-29 14:57:58 +0700

    forgot

--- backend/migrations/migration_orders.py
+++ backend/migrations/migration_orders.py
@@ -5,9 +5,10 @@ from backend.src.utils.table import Table
 # don't change this variable name
 TABLE_NAME = "orders"
 
+
 def up():
     table = Table(name=TABLE_NAME)
-    table.int("id").primary_key().auto_increment()
+    table.int("id").primary_key().auto_incremenwwwwwwwwwwwwwwwwwt()
     table.foreign_key("trackingNumber", "trackings", "id")
     table.foreign_key("productId", "products", "id")
     table.json("customization").default("'{}'")
@@ -15,11 +16,13 @@ def up():
     table.decimal("cost", precision=2)
     table.timestamp("createdAt").default("CURRENT_TIMESTAMP")
 
-    table.migrate() # always finish with a call to table.migrate()
+    table.migrate()  # always finish with a call to table.migrate()
     return table
-    
+
+
 def down(db_conn: msc.MySQLConnection):
     Table(name=TABLE_NAME).drop(db_conn)
+
 
 # if you need to seed the database
 def seed() -> list[dict]:
